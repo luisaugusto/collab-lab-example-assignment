934521959dc153c34be6b99ac2855e1dee2206d7
Luis Augusto <hello@luis.app>
2021-11-15 16:14:06 -0800

    Reformat app.js and index.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import './App.css';
+import "./App.css";
 import React from "react";
 
 function App() {
-  const [textInput, setTextInput] = React.useState('Here is some example text.');
-  const [conversionMode, setConversionMode] = React.useState('lowercase');
-  const [textOutput, setTextOutput] = React.useState('');
+  const [textInput, setTextInput] = React.useState(
+    "Here is some example text."
+  );
+  const [conversionMode, setConversionMode] = React.useState("lowercase");
+  const [textOutput, setTextOutput] = React.useState("");
 
-  const handleRadioChange = event => {
+  const handleRadioChange = (event) => {
     setConversionMode(event.target.value);
-  }
+  };
 
-  const handleTextareaChange = event => {
+  const handleTextareaChange = (event) => {
     setTextInput(event.target.value);
   };
 
-  const handleSubmit = event => {
+  const handleSubmit = (event) => {
     event.preventDefault();
-    setTextOutput('Your formatted text will go here!')
+    setTextOutput("Your formatted text will go here!");
   };
 
   return (
@@ -24,43 +26,43 @@ function App() {
       <header>
         <h1>Career Lab text-case converter</h1>
       </header>
-        <form onSubmit={handleSubmit}>
-          <div className="form-control form-control__text">
-            <label htmlFor="text">Text to be converted:</label>
-            <textarea
-              id="text"
-              onChange={handleTextareaChange}
-              value={textInput}
-            />
-          </div>
-          <div className="form-control form-control__radio">
-            <input
-              type="radio"
-              name="conversion"
-              id="conversion-0"
-              value="lowercase"
-              checked={conversionMode === "lowercase"}
-              onChange={handleRadioChange}
-            />
-            <label htmlFor="conversion-0">Convert text to lowercase</label>
-          </div>
-          <div className="form-control form-control__radio">
-            <input
-              type="radio"
-              name="conversion"
-              id="conversion-1"
-              value="uppercase"
-              checked={conversionMode === "uppercase"}
-              onChange={handleRadioChange}
-            />
-            <label htmlFor="conversion-1">Convert text to uppercase</label>
-          </div>
-          <input type="submit" value="Submit" />
-          <div className="result-wrapper form-control form-control__text">
-            <label htmlFor="result">Converted text:</label>
-            <output id="result">{textOutput}</output>
-          </div>
-        </form>
+      <form onSubmit={handleSubmit}>
+        <div className="form-control form-control__text">
+          <label htmlFor="text">Text to be converted:</label>
+          <textarea
+            id="text"
+            onChange={handleTextareaChange}
+            value={textInput}
+          />
+        </div>
+        <div className="form-control form-control__radio">
+          <input
+            type="radio"
+            name="conversion"
+            id="conversion-0"
+            value="lowercase"
+            checked={conversionMode === "lowercase"}
+            onChange={handleRadioChange}
+          />
+          <label htmlFor="conversion-0">Convert text to lowercase</label>
+        </div>
+        <div className="form-control form-control__radio">
+          <input
+            type="radio"
+            name="conversion"
+            id="conversion-1"
+            value="uppercase"
+            checked={conversionMode === "uppercase"}
+            onChange={handleRadioChange}
+          />
+          <label htmlFor="conversion-1">Convert text to uppercase</label>
+        </div>
+        <input type="submit" value="Submit" />
+        <div className="result-wrapper form-control form-control__text">
+          <label htmlFor="result">Converted text:</label>
+          <output id="result">{textOutput}</output>
+        </div>
+      </form>
     </div>
   );
 }
